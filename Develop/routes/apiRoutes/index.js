b2ea9cd709e1bce4ad674e8f215e0642d9201431
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-05-16 15:48:45 -0400

    added Get/Post/Delete api router calls to and from database

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,4 +1,42 @@
 const path = require("require");
 const db = require("../../db/db.json");
 const router = require("express").Router();
-const { validateNote, createNote, filterId } = require("../../lib/notes");
+const {
+  validateNote,
+  createNote,
+  filterId,
+  updateDataBase,
+} = require("../../lib/notes");
+
+//get all notes!
+router.get("/notes", (req, res) => {
+  let results = db;
+  res.json(results);
+});
+
+//create/add new note
+router.post("/notes", (req, res) => {
+  let id;
+  req.body.id = iq;
+
+  //no body in the note?
+  if (!validateNote(req.body)) {
+    res.status(400).send("Something looks wrong.... Try Again!");
+  } else {
+    const note = createNote(req.nody, db);
+    res.json(note);
+  }
+});
+
+//Bonus DELETE
+router.delete("/notes/:id", (req, res) => {
+  let filteredResults = filterId(req.params.id, db);
+
+  db = filteredResults;
+
+  //saving new copy and pushing to main file
+  updateDataBase(filteredResults);
+  res.json(filteredResults);
+});
+
+module.exports = router;
