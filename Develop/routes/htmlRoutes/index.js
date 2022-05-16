e30bb2b66c4fbcdafb234eda0453ea9551b67718
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-05-16 07:26:14 -0400

    completing server connections between main page and notes page

---
+++ Develop/routes/htmlRoutes/index.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const router = require("express").Router;
+
+//get Main page
+
+//get Notes Page
+
+//get Everything else * universal
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/notes.html"));
+});
+
+router.get("*", (req, res) => {
+  //anything else, go to index.html, main page!
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,19 @@
+//set up variables and require
 const express = require("express");
-
 const noteTakerRouter = require("./routes/apiRoutes/NotesRoutes");
-
 const PORT = process.env.PORT || 3001;
+
+//app use express
 const app = express();
 
+//congfiguration for using Express Server
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 //USE APIROUTES
 
+//app listen to port
 app.listen(PORT, () => {
   console.log(`API SERVER now on port ${PORT}`);
 });
